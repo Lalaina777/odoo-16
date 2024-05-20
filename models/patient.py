8c608e86af2df49f055b354b17647d2270ae167f
Lalaina777 <madawebzone@gmail.com>
2024-05-20 10:42:12 +0300

    model.py v2

--- models/patient.py
+++ models/patient.py
@@ -4,13 +4,15 @@ from odoo import api, fields, models
 
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
-    _description = "Hospital Patient"
+    _description = "Patient Records"
 
     name = fields.Char(string='Name', required=True)
     age = fields.Integer(string='Age')
+    is_child = fields.boolean(string="Is Child ?")
+    notes = fields.Text(string="Notes")
     gender = fields.Selection([
         ('male', 'Male'),
         ('female', 'Female'),
         ('other', 'Other'),
     ], required=True, default='male')
-    note = fields.Text(string='Description')
+    
